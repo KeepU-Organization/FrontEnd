f495588e71c2f83043bee7aaf48179d8ae95a92a
Ricardo Rivas <therickygame20@gmail.com>
2025-06-25 23:03:19 -0500

    Fix MonitorChildrenService
    Arreglo temporal de Monitor Children Service para poder desplegar el servicio para pruebas

--- src/services/MonitorChildrenService.tsx
+++ src/services/MonitorChildrenService.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {ChildSummary} from "../types/User.tsx";
-import {TransactionResponse} from "../types/Transactions.tsx";
+//import {TransactionResponse} from "../types/Transactions.tsx";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1/';
 
